025d9a16ef5d25c68c0c42182dccf772c3e815b4
ThiagoB36 <97063776+ThiagoB36@users.noreply.github.com>
2025-04-15 13:22:45 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -41,7 +41,7 @@ alignItems:'center'
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
 
-            styles:[`{ width: 150, height: 50, backgroundColor: "blue", alignItems:'center', justifyContent:'center', color:'white'}`],
+            styles:[`{ width: 150, height: 50, backgroundColor: "blue", alignItems:'center', justifyContent:'center'}`],
 
             functions:[async (...args) =>
  functions.funcGroup({ args, pass:{
@@ -58,7 +58,8 @@ alignItems:'center'
           ],
 
           arrStyles: [
-            { color: 'black', fontSize: 12, }
+            `{
+color:'white'}`
           ],
 
           children: [
