5daaf92ab7958e8bf47d17d451181df71cd65a66
ThiagoB36 <97063776+ThiagoB36@users.noreply.github.com>
2025-05-30 12:55:42 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -93,7 +93,7 @@ alignItems:'center'
 'lst2': [{name: "Tarefa 1", date: "21/03/25"},{name: "Tarefa 2", date: "21/03/25"},{name: "Tarefa 3", date: "24/03/25"}], 'lst3': [{name: "Task 1"},{name: "Task 2"}] } 
 , 
 'name': "Name:", 
-'dataToSet': {name: "Carlos", phone: "1111"}, 'newValue': "Teste 01" } 
+'dataToSet': {name: "Carlos", phone: "1111"}, 'newValue': "30-05-2025" } 
  
 } 
  );
